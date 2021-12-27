a3378b9070a392c7c2bbc908cb4afa4a16b6392a
joseph-aswath <josephaswath@gmail.com>
2021-12-27 12:38:50 +0530

    major update

--- package/index.js
+++ package/index.js
@@ -1,5 +1,57 @@
-function isJJA (string) {
+
+
+/*FIRST EVER VERSION'S CODE */
+/*function isJJA (string) {
     return string === 'JJA';
 }
+*/
 
-module.exports = isJJA; 
+/********************************************************************
+function jozcapz(sentence){
+    let capzed = [];
+
+    let words = sentence.split(" ");
+    console.log("our split sentence:",words);
+
+    words.forEach(word => {
+        let capzedWord = word.slice(0,1).toUpperCase() + word.splice(1);
+        capzed.push(capzedWord);
+    });
+
+    console.log(capzed);
+
+    let capzedConverted = capzed.join("");
+
+    console.log(capzedConverted); 
+}
+
+module.exports = jozcapz; 
+********************************************************************/
+
+//var sentence = "this is our test sentence";
+var sentence;
+console.log("\n",sentence,"\n");
+
+/* for loops instead of forEach*/
+function jozzie(sentence){
+    var words = sentence.split(" ");
+    console.log("\n","array of words:",words,"\n");
+    
+    let capz = [ ];
+    
+    for (var i = 0; i < words.length ; i++) {
+        words[i] = words[i].slice(0,1).toUpperCase() + words[i].slice(1);
+        capz.push(words[i]);
+    }
+    
+    console.log("\n","capitalized list:",capz,"\n");
+    var unity = capz.join(" ");
+    console.log("\n","final sentence:",unity,"\n");
+    
+    return unity;
+    
+}
+
+console.log("\n" ,jozzie(sentence));
+
+module.exports = {jozzie};
--- test/script.js
+++ test/script.js
@@ -1,3 +1,23 @@
+
+
+/*
 const tester = require('@joseph_aswath/sample-framework');
 
 console.log(tester("JJA"));
+*/
+/*********************************************************************/
+
+var jozzie = require('@joseph_aswath/sample-framework');
+
+var sentence = "testing this sentence";
+
+console.log("\n",sentence,"\n");
+
+console.log("\n","result:",jozzie(sentence));
+
+var final = jozzie(sentence);
+
+console.log("\n",final,"\n");
+
+
+
